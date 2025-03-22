5a0f76fb42f12fdac729f3484ee9bff0f8c7949e
Viktor Sopko <najetak.viktrixeu@gmail.com>
2025-03-22 20:37:58 +0100

    add google adsense.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Head from "next/head";
 
 const inter = Inter({ weight: ["400", "600", "700"], subsets: ["latin"] });
 
@@ -17,6 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8435095568140595"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className={cn(inter.className, "bg-blue-950")}>{children}</body>
     </html>
   );
