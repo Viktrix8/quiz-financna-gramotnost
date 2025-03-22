6b3439fa40a9644fd62c23582c2ca700aede54e2
Viktor Sopko <najetak.viktrixeu@gmail.com>
2025-03-22 20:41:56 +0100

    change google adsene.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ weight: ["400", "600", "700"], subsets: ["latin"] });
 
@@ -18,13 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8435095568140595"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8435095568140595"
+        crossOrigin="anonymous"
+      ></Script>
       <body className={cn(inter.className, "bg-blue-950")}>{children}</body>
     </html>
   );
